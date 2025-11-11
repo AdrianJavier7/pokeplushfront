86730a0ad46372de92b7cffc2249cfc18e1253dc
josemary22 <josemarimateos2005@gmail.com>
2025-11-11 16:39:50 +0100

    Inicio de proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'principal',
+    loadComponent: () => import('./principal/principal.component').then((m) => m.PrincipalComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'principal',
+    pathMatch: 'full',
+  },
+  {
+    path: 'productos',
+    loadComponent: () => import('./productos/productos.component').then((m) => m.ProductosComponent),
+  },
+];
