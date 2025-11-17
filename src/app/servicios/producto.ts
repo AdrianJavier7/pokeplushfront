8f025782d52e69694e93ccd02fe2b2220dc1dd6c
josemary22 <josemarimateos2005@gmail.com>
2025-11-17 19:09:27 +0100

    Cambios productos

--- src/app/servicios/producto.ts
+++ src/app/servicios/producto.ts
@@ -2,31 +2,20 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Producto} from '../modelos/Producto';
+import {ComunService} from './comun.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductoService {
   private apiUrl= 'http://localhost:8080/api/productos';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private comun: ComunService) { }
 
   getProducto(): Observable<Producto[]> {
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJ0b2tlbkRhdGFEVE8iOnsiZW1haWwiOiJsdWNpYUBnbWFpbC5jb20iLCJuaXZlbCI6IlVTVUFSSU8iLCJmZWNoYV9jcmVhY2lvbiI6MTc2MzE2MTU4NDI0NSwiZmVjaGFfZXhwaXJhY2lvbiI6MTc2MzE3MjM4NDI0NX19.P1qN1QdaoPaJu2IMzBf1GoGHBlXbOGSHZmSMRgoU3Lw'; // <- pega el token aquí
-
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-
-    return this.http.get<Producto[]>(this.apiUrl, { headers });
+    return this.http.get<Producto[]>(this.apiUrl, this.comun.autorizarPeticion());
   }
 
-  getProductoPorId(id: number): Observable<Producto>{
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJ0b2tlbkRhdGFEVE8iOnsiZW1haWwiOiJsdWNpYUBnbWFpbC5jb20iLCJuaXZlbCI6IlVTVUFSSU8iLCJmZWNoYV9jcmVhY2lvbiI6MTc2MzMwNzY2OTY2MiwiZmVjaGFfZXhwaXJhY2lvbiI6MTc2MzMxODQ2OTY2Mn19.oXYiaQChSxpZVEYqXyeqJrmeR0u8JdHlAzxMXrZXvco'; // <- pega el token aquí
-
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-    return this.http.get<Producto>(`${this.apiUrl}/${id}`, { headers });
+  getProductoPorId(id: number): Observable<Producto> {
+    return this.http.get<Producto>(`${this.apiUrl}/${id}`, this.comun.autorizarPeticion());
   }
-
 }
