de8df0b458a97fd2aa1e84297ea8203476dbd43d
AdrianJavier7 <perezhispalis10@gmail.com>
2025-11-20 16:42:55 +0100

    Carrito funcional

--- src/app/servicios/CarritoService.ts
+++ src/app/servicios/CarritoService.ts
@@ -3,6 +3,7 @@ import {Comun} from './comun';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Carrito} from '../modelos/Carrito';
+import {Item} from '../modelos/Item';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,21 @@ export class CarritoService {
   getCarrito(): Observable<Carrito> {
     const options = this.comun.autorizarPeticion();
     return this.http.get<any>('http://localhost:8080/carrito/obtener', options);
+  }
+
+  quitarItem(item: Item): Observable<Carrito> {
+    const options = this.comun.autorizarPeticion();
+    return this.http.post<Carrito>('http://localhost:8080/carrito/quitar', item, options);
+  }
+
+  obtenerPedidos(): Observable<Carrito[]> {
+    const options = this.comun.autorizarPeticion();
+    return this.http.get<Carrito[]>('http://localhost:8080/carrito/pedidos', options);
+  }
+
+  finalizarCarrito(): Observable<Carrito> {
+    const options = this.comun.autorizarPeticion();
+    return this.http.post<any>('http://localhost:8080/carrito/procesando', {}, options);
   }
 
 }
